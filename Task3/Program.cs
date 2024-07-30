000965c6459bc43ff1b8e9ea717eeeddaf688e96
Natalie <sonatalex@gmail.com>
2024-07-31 00:38:04 +0300

    3 task

---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿void Main()
+{
+ 
+ int[] array = new int[6];
+ Random rand = new Random();
+ Console.Write("Исходный массив: ");
+ for (int i = 0; i < array.Length; i++)
+ {
+ array[i] = rand.Next(0, 10); 
+ Console.Write(array[i] + " "); 
+ }
+
+Console.WriteLine();
+
+Console.Write("Перевернутый массив: ");
+ReverseArray(array, array.Length - 1);
+ Console.WriteLine();
+
+}
+
+void ReverseArray(int[] array, int i)
+{
+
+if (i < 0) return; 
+ Console.Write(array[i] + " ");
+ ReverseArray(array, i - 1);
+  
+}
+
+
+Main();
