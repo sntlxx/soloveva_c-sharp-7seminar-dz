52727245b0e839562ee5fc217e71a0264eb9133d
Natalie <sonatalex@gmail.com>
2024-07-31 00:35:06 +0300

    2 task

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿void Main()
+{
+int m = ReadInt("Введите число m: ");
+int n = ReadInt("Введите число n: ");
+Console.WriteLine($"Результат: {Akkerman(m, n)}");
+
+}
+
+int Akkerman(int m, int n)
+{
+    if (m ==0)
+        return (n + 1);
+    else if (m > 0 && n==0)
+        return Akkerman(m-1,1);
+    else
+        return Akkerman(m-1, Akkerman(m, n-1));
+
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
